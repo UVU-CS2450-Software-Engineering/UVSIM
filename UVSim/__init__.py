2f36da005c0cfe2fd9750f32ac543ad58107fdfa
Dallin Allred <dallin@qcwwmail.com>
2023-02-06 11:20:50 -0700

    Added driver functionality in __main__

--- UVSim/__init__.py
+++ UVSim/__init__.py
@@ -1,1 +1,2 @@
 import sys
+import UVSim.read_ml
--- UVSim/__main__.py
+++ UVSim/__main__.py
@@ -1,1 +1,36 @@
 # Place the code to run the package here
+
+from UVSim.read_ml import read_ml
+from UVSim.virtual_machine import VirtualMachine
+
+vm = VirtualMachine()
+
+program_path = input('Enter program path to run: ')
+
+vm.instructions = read_ml(program_path)
+
+while True:
+    result = vm.execute()
+    # Expected values for result from vm.execute()
+    # Successfully completed actions that require no IO - no return statement (or return None)
+    # Input required - return {input: True, text: 'Message describing required input: '}
+    # Output - return {output: True, text: 'Message/value to be displayed'}
+    # Program complete - return {complete: True}
+    # Error in execution - return {error: 'Descriptive error message'}
+
+    if result == None: continue
+    elif result.input:
+        data = input(result.text)
+        vm.executeInput(data)
+    elif result.output:
+        print(result.text)
+    elif result.complete:
+        print('Program complete. Press any key to continue...')
+        input()
+        break
+    else:
+        print(f'Error occurred:\n{result.error}')
+        print('-' * 20)
+        print('Press any key to continue...')
+        input()
+        break
