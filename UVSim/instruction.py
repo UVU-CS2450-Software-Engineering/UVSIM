f785ee18e97bc469ed52d9d4e2070e02368957a5
FuerteTurtle <davegoodrich45+gitkraken@gmail.com>
2023-02-06 13:33:32 -0700

    Added instruction abstract class and add child class example

--- UVSim/instruction.py
+++ UVSim/instruction.py
@@ -1,32 +1,34 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from virtual_machine import virtual_machine
+from UVSim import virtual_machine
+
 
 class instruction(ABC):
-  """
-  op_name:str
-  param:int
-  instr:int
-  """
-  @abstractmethod
-  def __init__(self:instruction,instr:int)->None:
     """
-    the constructor
+    op_name:str
+    param:int
+    instr:int
     """
-    self.instr:int = instr
-    self.op_code = int(str(instr)[:2])
-    self.param = int(str(instr[2:]))
-    self.op_name = ""
 
-  @abstractmethod
-  def exec(self:instruction, vm:virtual_machine):
-    """
-    defined by the child, do whatever the instruction does
-    """
-    pass
+    @abstractmethod
+    def __init__(self: instruction, instr: int) -> None:
+        """
+        the constructor
+        """
+        self.instr: int = instr
+        self.op_code = int(str(instr)[:2])
+        self.param = int(str(instr[2:]))
+        self.op_name = ""
 
-  def __str__(self:instruction) -> str:
-    """
-    overload the string operator for easy printing
-    """
-    return f"instruction:{self.instr} op_code:{self.op_code} param:{self.param} name:{self.op_name}"
+    @abstractmethod
+    def exec(self: instruction, vm: virtual_machine):
+        """
+        defined by the child, do whatever the instruction does
+        """
+        pass
+
+    def __str__(self: instruction) -> str:
+        """
+        overload the string operator for easy printing
+        """
+        return f"instruction:{self.instr} op_code:{self.op_code} param:{self.param} name:{self.op_name}"
