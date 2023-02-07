5123d6823690111606bed814f24255445fe8c33d
Ed <10942547@my.uvu.edu>
2023-02-07 16:32:24 -0700

    add IO and change vm

--- test/test_vm.py
+++ test/test_vm.py
@@ -4,18 +4,15 @@ from UVSim.vm import *
 
 
 def test_vm_accumulator():
-    acc = accumulator()
-    obj = virtualMachine(acc)
-    assert obj.vmAccumulator.value == 0
+    obj = virtualMachine()
+    assert obj.vmAccumulator == 0
 
 
 def test_vm_default_exit_value():
-    acc = accumulator()
-    obj = virtualMachine(acc)
+    obj = virtualMachine()
     assert obj.exit == False
 
 
 def test_vm_default_next_instruction_value():
-    acc = accumulator()
-    obj = virtualMachine(acc)
+    obj = virtualMachine()
     assert obj.nextInstruction == None
