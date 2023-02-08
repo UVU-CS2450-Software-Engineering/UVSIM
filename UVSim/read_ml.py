33af51aa77189b1ad4953ac1764e769220c79dfd
Dallin Allred <dallin@qcwwmail.com>
2023-02-07 21:29:09 -0700

    read_ml RegEx and buffer overflow checking

--- UVSim/read_ml.py
+++ UVSim/read_ml.py
@@ -1,3 +1,5 @@
+import re
+
 def read_ml(path):
     '''Reads in a supplied program file and stores each line as an element in a list'''
     instructions = []
@@ -5,9 +7,22 @@ def read_ml(path):
         with open(path, 'r') as prgm:
             instruction = prgm.readline().strip()
             while instruction:
+                print(len(instructions))
+                if len(instructions) > 100:
+                    raise BufferError('Memory Overflow: Too many lines in program.')
+                # Regex to validate format
+                if not re.search("^((\+|-)?\d{4})$", instruction):
+                    raise ValueError(f'Invalid word at line: {len(instructions) + 1}')
                 instructions.append(instruction)
                 instruction = prgm.readline().strip()
         return instructions
+    except BufferError as e:
+        print(e)
+        return False
     except OSError:
         print(f'Unable to open file at: {path}')
         return False
+    except ValueError as e:
+        print(e)
+        return False
+
