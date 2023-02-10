5970b2576d428436b09eb403c8009cd0df2cf30f
Dallin Allred <dallin@qcwwmail.com>
2023-02-10 10:42:48 -0700

    Updated regex and return type for read_ml to allow passing errors up the call stack

--- UVSim/read_ml.py
+++ UVSim/read_ml.py
@@ -11,18 +11,15 @@ def read_ml(path):
                 if len(instructions) > 100:
                     raise BufferError('Memory Overflow: Too many lines in program.')
                 # Regex to validate format
-                if not re.search("^((\+|-)\d{4})$", instruction):
+                if not re.search("^(([+]|-)\d{4})$", instruction):
                     raise ValueError(f'Invalid word at line: {len(instructions) + 1}')
                 instructions.append(instruction)
                 instruction = prgm.readline().strip()
-        return instructions
+        return {'result': instructions}
     except BufferError as e:
-        print(e)
-        return False
+        return {'error': e}
     except OSError:
-        print(f'Unable to open file at: {path}')
-        return False
+        return {'error': f'Unable to open path at: {path}'}
     except ValueError as e:
-        print(e)
-        return False
+        return {'error': e}
 
--- test/test_read_ml.py
+++ test/test_read_ml.py
@@ -4,20 +4,20 @@ from UVSim import read_ml
 
 def test_no_file():
     commands = read_ml.read_ml('No file')
-    assert not commands
+    assert commands['error']
 
 def test_dummy_file():
     commands = read_ml.read_ml('test/test1.txt')
-    assert len(commands) == 11
+    assert len(commands['result']) == 11
 
 def test_invalid_entry():
     commands = read_ml.read_ml('test/testInvalid.txt')
-    assert not commands
+    assert commands['error']
 
 def test_max_program():
     commands = read_ml.read_ml('test/testMax.txt')
-    assert len(commands) == 100
+    assert len(commands['result']) == 100
 
 def test_overflow():
     commands = read_ml.read_ml('test/testOverflow.txt')
-    assert not commands
+    assert commands['error']
