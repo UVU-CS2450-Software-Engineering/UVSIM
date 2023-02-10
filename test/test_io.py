404a4d5ca8f1a251fa787e01f4fcf19e359548bc
Dallin Allred <dallin@qcwwmail.com>
2023-02-10 16:49:29 -0700

    Updated regex and accumulator storage to use integers

--- test/test_io.py
+++ test/test_io.py
@@ -45,7 +45,7 @@ def test_read_accum_value(monkeypatch):
     instr = read("+1035")
     monkeypatch.setattr("builtins.input", lambda _: "+5240")
     instr.exec(obj)
-    assert obj.vmAccumulator == "+5240"
+    assert obj.vmAccumulator == 5240
 
 
 def test_read_memory_value(monkeypatch):
@@ -53,7 +53,7 @@ def test_read_memory_value(monkeypatch):
     instr = read("+1035")
     monkeypatch.setattr("builtins.input", lambda _: "+5240")
     instr.exec(obj)
-    assert obj.mainMemory[35] == "+5240"
+    assert obj.mainMemory[35] == 5240
 
 
 def test_read_regex():
