ad92d50277e05c8c941221bb2bccabefb58ef4fd
Ed <10942547@my.uvu.edu>
2023-02-10 19:06:52 -0700

    Add load and store to io.py and tests to test_io.py

--- UVSim/io.py
+++ UVSim/io.py
@@ -43,3 +43,37 @@ class read(instruction):
             raise ValueError(f"Invalid word")
         vm.vmAccumulator = int(inp)
         vm.mainMemory[self.param] = vm.vmAccumulator
+
+class load(instruction):
+    """
+    a class for the load instruction
+    """
+
+    def __init__(self: instruction, instr: int) -> None:
+        super().__init__(instr)
+        self.op_name: str = "LOAD"
+        assert (
+            self.op_code == 20
+        ), "Tried to create a load instruction with mismatched op code"
+
+    # get word from memorylocation, move it into the accumulator
+    def exec(self: instruction, vm: virtualMachine):
+        # self.param is location in memory of operand to write
+        vm.vmAccumulator = vm.mainMemory[self.param]
+
+class store(instruction):
+    """
+    a class for the store instruction
+    """
+
+    def __init__(self: instruction, instr: int) -> None:
+        super().__init__(instr)
+        self.op_name: str = "STORE"
+        assert (
+            self.op_code == 21
+        ), "Tried to create a store instruction with mismatched op code"
+
+    #Store a word from the accumulator into a specific location in memory.
+    def exec(self: instruction, vm: virtualMachine):
+        # self.param is location in memory of destination
+        vm.mainMemory[self.param] = vm.vmAccumulator
